4b858793a586f92ec7307d798b4f666a34085ca4
shayro13 <shayro13@gmail.com>
2019-05-10 22:01:37 +0300

    test

--- Assets/scripts/movement/Jump.cs
+++ Assets/scripts/movement/Jump.cs
@@ -9,19 +9,17 @@ public class Jump : MonoBehaviour
     private BoxCollider2D col;
     //checks if you can jump again
     private bool grounded;
-    LayerMask mask;
+    LayerMask mask = 8;
     private bool lastFrameReleaseButton = false;
     private Rigidbody2D rb;
     //set jump speed and how fast you fall when stopped pressing
     public float jumpspeed = 10;
     public float jumpDecay = 0.5f;
-    public float onAirJump = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        mask = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
@@ -30,7 +28,7 @@ public class Jump : MonoBehaviour
         Vector2 jump = Vector2.zero;
         jump.y = Input.GetAxis("Jump");
         //checks if you touch a ground layer object
-        grounded = col.IsTouchingLayers(mask);
+        grounded = col.IsTouchingLayers();
         if (jump.y > 0 && grounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jump.y * jumpspeed);
---
+++ Library/Collab/Original/Assets/scripts/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    //player speed
+    public float speed = 10;
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //rfgrgqergqerg
+        if (!(rb.velocity.x != 0 && Input.GetAxis("Horizontal") == 0))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        }
+    }
+}
